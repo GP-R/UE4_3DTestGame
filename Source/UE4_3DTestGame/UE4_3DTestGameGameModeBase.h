dee956d3e0da643d7e2476387d261c59afc3ccc8
GP-R <seonwoo3644@naver.com>
2023-01-04 19:36:15 +0900

    Initial commit

---
+++ Source/UE4_3DTestGame/UE4_3DTestGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_3DTestGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_3DTESTGAME_API AUE4_3DTestGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
